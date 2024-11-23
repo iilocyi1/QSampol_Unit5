c459093de5847d8330194592c82b717db4ae4470
iilocyi1 <124301687+iilocyi1@users.noreply.github.com>
2024-11-22 22:20:41 -0800

    LessonD

--- QSampol_Unit5/Assets/Scripts/Target.cs
+++ QSampol_Unit5/Assets/Scripts/Target.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class Target : MonoBehaviour
 {
@@ -62,9 +62,5 @@ public class Target : MonoBehaviour
         }
     }
 
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-    }
+   
 }
